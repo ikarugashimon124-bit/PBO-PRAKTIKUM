f8e5131d8c3b74ea2c6798919484a7702ee1aabe
ikarugashimon124-bit <ikarugashimon124@gmail.com>
2026-06-17 22:04:14 +0700

    Add files via upload

--- PBO-7/Mahasiswa.java
+++ PBO-7/Mahasiswa.java
@@ -1,108 +1,45 @@
 /* Nama File    : Mahasiswa.java
- * Deskripsi   : Kelas Mahasiswa yang mewarisi Civitasakademika, dengan method overloading setProgramStudi, setWali, dan tampilDataMahasiswa
+ * Deskripsi   : Kelas Mahasiswa yang mewarisi kelas Civitasakademika
  * Pembuat     : Mutiara Ayu Pramono - 24060123140131
  * Tanggal     : 17 Juni 2026
  */
 
 public class Mahasiswa extends Civitasakademika {
     /******************ATRIBUT*****************/
-    String NIM;      
-    String Programstudi;
-    Dosen  wali;  
+    String NIM;
+    Dosen  Dosenwali;
 
     /******************KONSTRUKTOR*************/
-    Mahasiswa() {
-        super("n/a");
-        NIM = "n/a";
-        Programstudi = "n/a";
-        wali = null;
-    }
-
     Mahasiswa(String NIM, String Nama) {
         super(Nama);
-        this.NIM = NIM;
-        this.Programstudi = "n/a";
-        this.wali = null;
-    }
-
-    Mahasiswa(String NIM, String Nama, String Programstudi) {
-        super(Nama);
-        this.NIM = NIM;
-        this.Programstudi = Programstudi;
-        this.wali = null;
-    }
-
-    Mahasiswa(int NIM, String Nama, String Programstudi) {
-        super(Nama);
-        this.NIM = String.valueOf(NIM);
-        this.Programstudi = Programstudi;
-        this.wali = null;
-    }
-
-    Mahasiswa(Mahasiswa M) {
-        super(M.Nama);
-        this.NIM          = M.NIM;
-        this.Programstudi = M.Programstudi;
-        this.wali         = M.wali;
+        this.NIM       = NIM;
+        this.Dosenwali = null; // belum memiliki dosenwali
     }
 
     /******************METHOD******************/
-    @Override
+    // Mengembalikan NIM sebagai nomor identitas mahasiswa
     String getNomor() {
         return NIM;
     }
 
-    void setNIM(String NIM) {
-        this.NIM = NIM;
-    }
-
-    void setNama(String Nama) {
-        this.Nama = Nama;
-    }
-
-    void setWali(Dosen wali) {
-        this.wali = wali;
-    }
-
-    void setProgramStudi() {
-        Programstudi = "Kosong";
-    }
-
-    void setProgramStudi(String prodi) {
-        Programstudi = prodi;
-    }
-
-    void setProgramStudi(Mahasiswa M) {
-        Programstudi = M.Programstudi;
-    }
-
+    // Mengembalikan nilai NIM
     String getNIM() {
         return NIM;
     }
 
-    String getProgramstudi() {
-        return Programstudi;
+    // Mengubah isian komponen Dosenwali dengan parameter input Dosen
+    void setWali(Dosen D) {
+        Dosenwali = D;
     }
 
-    Dosen getWali() {
-        return wali;
-    }
-
-    void printMahasiswa() {
-        System.out.println("NIM: " + NIM);
-        System.out.println("Nama: " + Nama);
-        System.out.println("Program Studi: " + Programstudi);
-    }
-
+    // Menampilkan NIM, Nama Mahasiswa, dan Nama Dosenwali
     void tampilDataMahasiswa() {
-        System.out.println("NIM: " + NIM);
-        System.out.println("Nama: " + Nama);
-        System.out.println("Program Studi: " + Programstudi);
-        if (wali != null) {
-            System.out.println("Dosen Wali: " + wali.getNama()
-                               + " (NIP: " + wali.getNIP() + ")");
+        System.out.println("NIM        : " + NIM);
+        System.out.println("Nama       : " + Nama);
+        if (Dosenwali != null) {
+            System.out.println("Dosen Wali : " + Dosenwali.getNama());
         } else {
-            System.out.println("Dosen Wali: -");
+            System.out.println("Dosen Wali : (belum ditentukan)");
         }
     }
 }
