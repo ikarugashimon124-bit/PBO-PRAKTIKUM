64b1efc26d22dc73b17060d3b752d7602a4d506d
ikarugashimon124-bit <ikarugashimon124@gmail.com>
2026-06-17 21:48:27 +0700

    Add files via upload

---
+++ PBO-3-FINAL/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+    Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
+    Titik T2 = new Titik(3,5); //Membuat objek titik T2 (3,5)
+    System.out.println("Jumlah Objek Titik = " + Titik.getCounterTitik());
+    System.out.println("Jumlah Objek Titik = " + T2.getCounterTitik());
